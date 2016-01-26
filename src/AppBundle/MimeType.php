e6559a71fcffb30b1411eb2c27c8f2baa119c7dd
Kristan Webb <kristan@webbitpro.co.uk>
2016-01-26 21:04:00 +0000

    update playlist controller to return cached image urls.

---
+++ src/AppBundle/MimeType.php
@@ -0,0 +1,15 @@
+<?php
+namespace AppBundle;
+
+class MimeType {
+    
+    
+    
+    public $type;
+    
+    public function __construct() {
+        
+        $this->type['TYPE_IMAGE'] = 'image';
+        $this->type['TYPE_VIDEO'] = 'video';
+    }
+}
