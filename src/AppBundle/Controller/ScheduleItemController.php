8951cb2edd8cd6269892c513e53d342ac9024988
Kristan Webb <kristan@webbitpro.co.uk>
2016-02-07 20:44:56 +0000

    ordering logic / add add form to schedule / delete schedule item link

--- src/AppBundle/Controller/ScheduleItemController.php
+++ src/AppBundle/Controller/ScheduleItemController.php
@@ -3,11 +3,13 @@
 namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\ScheduleItem;
 use AppBundle\Form\ScheduleItemType;
+use Doctrine\ORM\Query;
 
 /**
  * ScheduleItem controller.
@@ -32,6 +34,93 @@ class ScheduleItemController extends Controller
             'scheduleItems' => $scheduleItems,
         ));
     }
+    
+    
+    
+   
+    /**
+     * moves a ScheduleItem entity.
+     *
+     * @Route("/move/{id}/{direction}", name="scheduleitem_move")
+     */
+    public function moveAction(Request $request, ScheduleItem $scheduleItem, $direction)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $current =  $em->getRepository('AppBundle:ScheduleItem')->findOneBy(array('id' => $scheduleItem));
+
+        
+        
+        $offset = ( $direction == 'up' ? -1 : +1 );
+        $dir = ( $direction == 'up' ? '<' : '>' );
+        $sort = ( $direction == 'up' ? 'DESC' : 'ASC' );
+        //$swap = $em->getRepository('AppBundle:ScheduleItem')->findBySequence($scheduleItem->getSequence() + $offset)->findByChannel($scheduleItem->getChannel());
+       
+        $repository = $this->getDoctrine()->getRepository('AppBundle:ScheduleItem');
+        $query = $repository->createQueryBuilder('si')
+                            ->where('si.channel = :cha')
+                            ->andWhere("si.sequence $dir :seq")
+                            ->setParameter('cha',  $scheduleItem->getChannel())
+                            ->setParameter('seq',  $scheduleItem->getSequence())
+                            ->orderBy('si.sequence', $sort)
+                            ->setMaxResults(1)
+                            ->getQuery();
+
+        $swap = $query->getResult();
+        
+//        $swap = $em->getRepository('AppBundle:ScheduleItem')
+//            ->findOneBy(
+//            array(
+//                'sequence' => $scheduleItem->getSequence() + $offset ,
+//                'channel' => $scheduleItem->getChannel()
+//                 ),
+//            array(
+//                'sequence' => 'DESC'
+//            )
+//        );
+        
+        
+        //->where('channel == :ch')->setParameter('ch', $scheduleItem->getChannel());
+        //$em->persist($scheduleItem);
+        //  $em->flush();
+        
+        if ($swap)
+        {
+
+            //dump($current);
+            //dump($swap[0]);
+            $cuS = $current->getSequence();
+            $swS = $swap[0]->getSequence();
+
+
+
+            $current->setSequence($swS);
+            $swap[0]->setSequence($cuS);
+
+            $em->persist($current);
+            $em->persist($swap[0]);
+            $em->flush();
+            
+        }
+        
+        
+        //dump($current);
+        //dump($swap);
+        //  return new Response();
+
+        return $this->redirectToRoute('channel_schedule', array( 'id' => $scheduleItem->getChannel()->getId() ));
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     /**
      * Creates a new ScheduleItem entity.
@@ -46,11 +135,23 @@ class ScheduleItemController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            
+            $em = $this->getDoctrine()->getManager();
+            $highest = $em->createQueryBuilder()
+                ->select('MAX(e.sequence)')
+                ->from('AppBundle:ScheduleItem', 'e')
+                ->where('e.channel = :ch')
+                ->setParameter('ch', $scheduleItem->getChannel())
+                ->getQuery()
+                ->getSingleScalarResult();
+            
+            
+            $scheduleItem->setSequence($highest + 1);
             $em = $this->getDoctrine()->getManager();
             $em->persist($scheduleItem);
             $em->flush();
 
-            return $this->redirectToRoute('scheduleitem_show', array('id' => $scheduleitem->getId()));
+            return $this->redirectToRoute('channel_schedule', array('id' => $scheduleItem->getChannel()->getId()));
         }
 
         return $this->render('scheduleitem/new.html.twig', array(
@@ -121,6 +222,24 @@ class ScheduleItemController extends Controller
 
         return $this->redirectToRoute('scheduleitem_index');
     }
+    
+    
+        /**
+     * Deletes a ScheduleItem entity.
+     *
+     * @Route("/{id}/delete", name="scheduleitem_delete_direct")
+     */
+    public function deleteDirectAction(Request $request, ScheduleItem $scheduleItem)
+    {
+      
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($scheduleItem);
+            $em->flush();
+        
+        return $this->redirectToRoute('channel_schedule', array('id' => $scheduleItem->getChannel()->getId()));
+    }
+    
+    
 
     /**
      * Creates a form to delete a ScheduleItem entity.
