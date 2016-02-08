cc26e3962d3e1465e77166b2da2be670a792c123
Kristan Webb <kristan@webbitpro.co.uk>
2016-02-08 08:29:44 +0000

    add date time picker to schedule item

--- src/AppBundle/Form/ScheduleItemType.php
+++ src/AppBundle/Form/ScheduleItemType.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +17,38 @@ class ScheduleItemType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $date = new \DateTime();
         $builder
-            ->add('start', 'datetime')
-            ->add('stop', 'datetime')
-            ->add('duration')
+            ->add('start', 'datetime',
+                  [
+                      'widget' => 'single_text',
+                      'format' => 'dd-MM-yyyy HH:00',
+                      'attr' => [
+                          'class' => 'form-control input-inline datetimepicker',
+                          'data-provide' => 'datetimepicker',
+                          'data-date-format' => 'dd-mm-yyyy hh:ii'
+                      ],
+                      'data' => $date
+                  ]
+                 )
+            ->add('stop', 'datetime',
+                  [
+                      'widget' => 'single_text',
+                      'format' => 'dd-MM-yyyy HH:00',
+                      'attr' => [
+                          'class' => 'form-control input-inline datetimepicker',
+                          'data-provide' => 'datetimepicker',
+                          'data-date-format' => 'dd-mm-yyyy hh:ii'
+                      ],
+                      'data' => $date->add(new \DateInterval('P1M'))
+                  ]
+                 )
+            ->add('duration', NumberType::class,
+                  [
+                      'max_length' => 2,
+                      'required'    => false,
+                      'empty_data'  => '',
+            ])
             ->add('channel')
             ->add('asset')
         ;
