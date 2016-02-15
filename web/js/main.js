e53e6645e1ae8dac2d58f671b98848c791535b4d
Kristan Webb <kristan@webbitpro.co.uk>
2016-02-15 21:24:21 +0000

    add javascript

---
+++ web/js/main.js
@@ -0,0 +1,18 @@
+
+ $( document ).ready(function() {
+     
+     
+     
+     
+     
+     
+     
+     
+  });
+
+
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+}); 
+
