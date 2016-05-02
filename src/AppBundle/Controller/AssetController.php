719a7041d392c6ed8e4f4d1bcdc446feada5478d
Kristan Webb <kristan@webbitpro.co.uk>
2016-05-02 19:39:26 +0100

    15 assets per page

--- src/AppBundle/Controller/AssetController.php
+++ src/AppBundle/Controller/AssetController.php
@@ -34,7 +34,7 @@ class AssetController extends Controller
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
-            10/*limit per page*/
+            15/*limit per page*/
         );
         
         
