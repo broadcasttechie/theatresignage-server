835655b5e1763949368dcb49af5797768d0da8a9
Kristan Webb <kristan@webbitpro.co.uk>
2020-01-12 21:26:52 +0000

    cleardb params

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -1,8 +1,8 @@
 <?php
-    $db = parse_url($_ENV['CLEARDB_DATABASE_URL']); 
+    $db = parse_url(getenv['CLEARDB_DATABASE_URL']); 
     $container->setParameter('database_driver', 'pdo_mysql'); 
     $container->setParameter('database_host', $db['host']); 
-    $container->setParameter('database_port', '~');
+    $container->setParameter('database_port', null);
     $container->setParameter('database_name', trim($db['path'], '/')); 
     $container->setParameter('database_user', $db['user']); 
     $container->setParameter('database_password', $db['pass']);
