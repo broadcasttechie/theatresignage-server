e8199e39e465e06fc988b460e87b44255b6c755c
Kristan Webb <kristan@webbitpro.co.uk>
2020-01-12 21:20:49 +0000

    cleardb prod vars

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -1,12 +1,13 @@
 <?php
-    $db = parse_url($_ENV('CLEARDB_DATABASE_URL'));
-
-    $container->setParameter('database_driver', 'pdo_mysql');
-    $container->setParameter('database_host', $db['host']);
-    $container->setParameter('database_port', $db['port']);
-    $container->setParameter('database_name', substr($db["path"], 1));
-    $container->setParameter('database_user', $db['user']);
+    $db = parse_url($_ENV['CLEARDB_DATABASE_URL']); 
+    $container->setParameter('database_driver', 'pdo_mysql'); 
+    $container->setParameter('database_host', $db['host']); 
+    $container->setParameter('database_port', '~');
+    $container->setParameter('database_name', trim($db['path'], '/')); 
+    $container->setParameter('database_user', $db['user']); 
     $container->setParameter('database_password', $db['pass']);
+    
+
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'en');
     $container->setParameter('mailer_transport', null);
