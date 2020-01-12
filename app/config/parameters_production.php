6cd17c5843cbdecf26bc9e62b76ac7dfa87308c7
Kristan Webb <kristan@webbitpro.co.uk>
2020-01-12 21:12:49 +0000

    Production parameters

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -1,5 +1,5 @@
 <?php
-    $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $db = parse_url($_ENV('CLEARDB_DATABASE_URL'));
 
     $container->setParameter('database_driver', 'pdo_mysql');
     $container->setParameter('database_host', $db['host']);
